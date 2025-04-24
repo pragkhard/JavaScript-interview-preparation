bd85b7d07ef5e1cd9031d286abc05dd167b3698e
Pragati <pragatikhard@gmail.com>
2025-04-24 22:35:31 +0530

    map

--- Map-Filter-Reduce/index.js
+++ Map-Filter-Reduce/index.js
@@ -1,29 +1,11 @@
 // MAP, FILTER & REDUCE Interview Questions
 
-// Question 1 : Array.map()
-
-
-
-
-
-// Question 2 : Array.filter()
-
-const nums = [1, 2, 3, 4];
-
-const moreThanTwo = nums.filter((num, i, arr) => num > 2 === 0);
-console.log(moreThanTwo);
 
 
 
 // Question 3 : Array.reduce()
 
-const nums = [1, 2, 3, 4];
 
-const sum = nums.reduce((acc, curr, i, arr) => {
-  return acc + curr;
-}, 0);
-
-console.log(sum); // 10
 
 
 
